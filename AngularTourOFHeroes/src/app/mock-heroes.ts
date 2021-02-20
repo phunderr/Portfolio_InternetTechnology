1ce90c95d77c1a0144dcb5657630b729c0d78262
Phunderr <Phunderr>
2021-02-20 16:18:18 +0100

    [added] Tour of heroes listing
    Already made the tutorial to this part

---
+++ AngularTourOFHeroes/src/app/mock-heroes.ts
@@ -0,0 +1,15 @@
+import { Hero } from './hero'
+
+export const HEROES: Hero[] = 
+[
+    {id: 2, name: 'Iron Man'},
+    {id: 3, name: 'Thor'},
+    {id: 4, name: 'spider man'},
+    {id: 5, name: 'Hulk'},
+    {id: 6, name: 'Black Widow'},
+    {id: 7, name: 'Scarlet witch'},
+    {id: 8, name: 'Vision'},
+    {id: 9, name: 'Ant man'},
+    {id: 10, name: 'Wasp'},
+    {id: 11, name: 'Why is Gemora'},
+];
